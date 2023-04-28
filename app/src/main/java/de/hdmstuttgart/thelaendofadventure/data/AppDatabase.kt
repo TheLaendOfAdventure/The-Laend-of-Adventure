498513e8350da89e425d2c0c95f3c55d61a90c82
Hausch Johannes <jh285@hdm-stuttgart.de>
2023-04-28 10:48:58 +0200

    LOA-223 Add prepopulation

--- app/src/main/java/de/hdmstuttgart/thelaendofadventure/data/AppDatabase.kt
+++ app/src/main/java/de/hdmstuttgart/thelaendofadventure/data/AppDatabase.kt
@@ -46,7 +46,7 @@ abstract class AppDatabase : RoomDatabase() {
                      * attempts to perform a migration with no defined migration path.
                      */
                     .fallbackToDestructiveMigration()
-                    .createFromAsset("Laend_of_Adventure.db")
+                    .createFromAsset("database/Laend_of_Adventure.db")
                     .build()
                     .also { Instance = it }
             }

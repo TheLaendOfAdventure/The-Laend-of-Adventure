395f9f87c4585866a119b301439cbe837a8642ee
Hausch Johannes <jh285@hdm-stuttgart.de>
2023-05-09 13:12:39 +0200

    LOA-253 Add LocationGoal helper class

---
+++ app/src/main/java/de/hdmstuttgart/thelaendofadventure/data/dao/datahelper/LocationGoal.kt
@@ -0,0 +1,8 @@
+package de.hdmstuttgart.thelaendofadventure.data.dao.datahelper
+
+data class LocationGoal(
+    val questID: Int,
+    val goalID: Int,
+    val latitude: Double,
+    val longitude: Double
+)

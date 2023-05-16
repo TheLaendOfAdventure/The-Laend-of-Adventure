6f1e25de35f1607344f25dc1b05888ff3396d67a
Aylin Alagöz <aa104@hdm-stuttgart.de>
2023-05-16 16:50:16 +0200

    LOA-230 Update UserPageFragment

--- app/src/main/java/de/hdmstuttgart/thelaendofadventure/ui/fragments/UserPageFragment.kt
+++ app/src/main/java/de/hdmstuttgart/thelaendofadventure/ui/fragments/UserPageFragment.kt
@@ -1,26 +1,44 @@
 package de.hdmstuttgart.thelaendofadventure.ui.fragments
 
+import android.app.AlertDialog
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import de.hdmstuttgart.the_laend_of_adventure.R
 import de.hdmstuttgart.the_laend_of_adventure.databinding.FragmentUserPageBinding
+import de.hdmstuttgart.thelaendofadventure.data.entity.UserEntity
+import de.hdmstuttgart.thelaendofadventure.permissions.PermissionManager
+import de.hdmstuttgart.thelaendofadventure.permissions.Permissions
 import de.hdmstuttgart.thelaendofadventure.ui.viewmodels.UserPageViewModel
-import java.io.File
 
 class UserPageFragment : Fragment(R.layout.fragment_user_page) {
 
     private lateinit var binding: FragmentUserPageBinding
     private lateinit var viewModel: UserPageViewModel
-
+    private lateinit var mPickGallery: ActivityResultLauncher<String>
+    private lateinit var permissionManager: PermissionManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Initialize ActivityResultLauncher to pick an image from the gallery
+        mPickGallery =
+            registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+                uri?.let {
+                    viewModel.saveImage(uri)
+                    binding.userPageProfilePictureView.setImageURI(viewModel.imageUri)
+                    Log.d(TAG, "User avatar image saved: $uri")
+                }
+            }
+        permissionManager = PermissionManager(requireContext())
     }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -35,18 +53,48 @@ class UserPageFragment : Fragment(R.layout.fragment_user_page) {
 
         viewModel = ViewModelProvider(this)[UserPageViewModel::class.java]
 
-        // User data is retrieved
-        viewModel.getUserData()
+        val userObserver = Observer<UserEntity> { user ->
+            binding.userPageNameView.text = user.name
+            binding.userPageLevelDisplay.text = user.level.toString()
+            binding.userPageProfileButtonLevelDisplay.text = user.level.toString()
+            binding.userPageProfilePictureView.setImageURI(user.imagePath?.toUri())
+        }
+        viewModel.user.observe(viewLifecycleOwner, userObserver)
 
-        // User data is bound to the XML Layout objects
-        binding.userPageNameView.text = viewModel.name
-        binding.userPageLevelDisplay.text = viewModel.level.toString()
-        binding.userPageProfileButtonLevelDisplay.text = viewModel.level.toString()
-        val imageUri = Uri.fromFile(File(viewModel.imagePath))
-        binding.userPageProfilePictureView.setImageURI(imageUri)
-
+        changeImageButton()
         setUpUserPageExitButton()
-        setUpChangeButton()
+    }
+
+    private fun changeImageButton() {
+        binding.userPageProfilePictureView.setOnClickListener {
+            if (permissionManager.checkPermission(Permissions.READ_WRITE_STORAGE)) {
+                pickImage()
+            } else {
+                Log.d(TAG, "READ_WRITE_STORAGE permission not granted")
+            }
+        }
+    }
+
+    private fun pickImage() {
+        val items = arrayOf<CharSequence>(
+            getString(R.string.pick_from_gallery),
+            getString(R.string.cancel)
+        )
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle(R.string.add_image)
+        builder.setItems(items) { dialog, item ->
+            when {
+                items[item] == getString(R.string.pick_from_gallery) -> {
+                    mPickGallery.launch("image/*")
+                    Log.d(TAG, "Launching gallery intent")
+                }
+
+                items[item] == getString(R.string.cancel) -> {
+                    dialog.dismiss()
+                }
+            }
+        }
+        builder.show()
     }
 
     private fun setUpUserPageExitButton() {
@@ -55,10 +103,7 @@ class UserPageFragment : Fragment(R.layout.fragment_user_page) {
         }
     }
 
-    /**
-     * This method changes the TextViews to User Input Fields and creates a confirm button*/
-    private fun setUpChangeButton() {
-        // A Button is needed in the XML File
-        // binding.userPageSettingsButton.
+    companion object {
+        private const val TAG = "UserPageFragment"
     }
 }

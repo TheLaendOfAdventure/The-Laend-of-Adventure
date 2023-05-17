c3462f254ead7dd71a6c9b9f1a0057d5f1eb0c9e
Konstantinos Gimoussiakakis <st172538@stud.uni-stuttgart.de>
2023-05-17 22:07:54 +0200

    LOA-201 update BadgesPageViewModel

--- app/src/main/java/de/hdmstuttgart/thelaendofadventure/ui/viewmodels/BadgesPageViewModel.kt
+++ app/src/main/java/de/hdmstuttgart/thelaendofadventure/ui/viewmodels/BadgesPageViewModel.kt
@@ -16,15 +16,5 @@ class BadgesPageViewModel(application: Application) : AndroidViewModel(applicati
         Context.MODE_PRIVATE
     ).getInt(R.string.userID.toString(), -1)
 
-    val badges = badgeRepository.getAcceptedBadgesByUserID(userID).asLiveData()
-
-    /*val badges: LiveData<List<BadgeEntity>> = MutableLiveData()
-
-    init {
-        fetchBadges()
-    }
-
-    private fun fetchBadges() {
-        badgeRepository.getAcceptedBadgesByUserID()
-    }*/
+    val badges = badgeRepository.getUnacceptedBadgesByUserID(userID).asLiveData()
 }

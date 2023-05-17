17375ab1d53ced18cb2fc5bd5aebe7c9c9f7861e
Konstantinos Gimoussiakakis <st172538@stud.uni-stuttgart.de>
2023-05-17 22:47:58 +0200

    LOA-201 update BadgesPageFragment

--- app/src/main/java/de/hdmstuttgart/thelaendofadventure/ui/fragments/BadgesPageFragment.kt
+++ app/src/main/java/de/hdmstuttgart/thelaendofadventure/ui/fragments/BadgesPageFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import de.hdmstuttgart.the_laend_of_adventure.R
 import de.hdmstuttgart.the_laend_of_adventure.databinding.FragmentBadgesPageBinding
 import de.hdmstuttgart.thelaendofadventure.data.entity.BadgeEntity
+import de.hdmstuttgart.thelaendofadventure.data.entity.UserEntity
 import de.hdmstuttgart.thelaendofadventure.ui.adapters.BadgesAdapter
 import de.hdmstuttgart.thelaendofadventure.ui.viewmodels.BadgesPageViewModel
 
@@ -40,6 +42,12 @@ class BadgesPageFragment : Fragment() {
             val adapter = BadgesAdapter(badge)
             recycleView.adapter = adapter
         }
+
+        val userObserver = Observer<UserEntity> { user ->
+            binding.badgesProfileButtonLevelDisplay.text = user.level.toString()
+            binding.badgesPageProfileButton.setImageURI(user.imagePath?.toUri())
+        }
+        viewModel.user.observe(viewLifecycleOwner, userObserver)
 
         // Observe the LiveData, passing in this activity as the LifecycleOwner and the observer.
         viewModel.badges.observe(viewLifecycleOwner, badgeObserver)

a4f0aaed8c4eb7af7a3eb3cf8bcad8602a15ead7
Konstantinos Gimoussiakakis <st172538@stud.uni-stuttgart.de>
2023-05-17 16:24:26 +0200

    LOA-268 moved into ui package and made asynchronous

--- app/src/main/java/de/hdmstuttgart/thelaendofadventure/ui/adapters/BadgesAdapter.kt
+++ app/src/main/java/de/hdmstuttgart/thelaendofadventure/ui/adapters/BadgesAdapter.kt
@@ -1,8 +1,8 @@
-package de.hdmstuttgart.thelaendofadventure.adapters
+package de.hdmstuttgart.thelaendofadventure.ui.adapters
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.LayoutInflater
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.net.toUri
@@ -10,38 +10,44 @@ import androidx.recyclerview.widget.RecyclerView
 import de.hdmstuttgart.the_laend_of_adventure.R
 import de.hdmstuttgart.thelaendofadventure.data.entity.BadgeEntity
 
-class BadgesAdapter(private val mList: List<BadgeEntity>) : RecyclerView.Adapter<BadgesAdapter.ViewHolder>() {
+class BadgesAdapter : RecyclerView.Adapter<BadgesAdapter.ViewHolder>() { // ktlint-disable max-line-length
+
+    private var badgeList = mutableListOf<BadgeEntity>()
+
+    fun submitList(newBadges: List<BadgeEntity>) {
+        badgeList.clear()
+        badgeList.addAll(newBadges)
+        notifyDataSetChanged()
+    }
 
     // create new views
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         // inflates the card_view_design view
         // that is used to hold list item
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.badges_page, parent, false)
+            .inflate(R.layout.fragment_badges_page, parent, false)
 
         return ViewHolder(view)
     }
 
     // binds the list items to a view
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-
-        val badges = mList[position]
+        val badges = badgeList[position]
 
         // sets the image to the imageview from our itemHolder class
         holder.imageView.setImageURI(badges.imagePath.toUri())
 
         // sets the text to the textview from our itemHolder class
         holder.textView.text = badges.name
-
     }
 
     // return the number of the items in the list
     override fun getItemCount(): Int {
-        return mList.size
+        return badgeList.size
     }
 
     // Holds the views for adding it to image and text
-    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val imageView: ImageView = itemView.findViewById(R.id.badge_image)
         val textView: TextView = itemView.findViewById(R.id.badge_name)
     }

a66a9f3dbe92e84817b532ed610c8c8e2105a180
Konstantinos Gimoussiakakis <st172538@stud.uni-stuttgart.de>
2023-05-17 22:48:07 +0200

    LOA-201 update BadgesPageViewModel

--- app/src/main/java/de/hdmstuttgart/thelaendofadventure/ui/viewmodels/BadgesPageViewModel.kt
+++ app/src/main/java/de/hdmstuttgart/thelaendofadventure/ui/viewmodels/BadgesPageViewModel.kt
@@ -7,9 +7,11 @@ import androidx.lifecycle.asLiveData
 import de.hdmstuttgart.the_laend_of_adventure.R
 import de.hdmstuttgart.thelaendofadventure.data.AppDataContainer
 import de.hdmstuttgart.thelaendofadventure.data.repository.BadgeRepository
+import de.hdmstuttgart.thelaendofadventure.data.repository.UserRepository
 
 class BadgesPageViewModel(application: Application) : AndroidViewModel(application) {
     private val badgeRepository: BadgeRepository = AppDataContainer(application).badgeRepository
+    private val userRepository: UserRepository = AppDataContainer(application).userRepository
 
     val userID = application.getSharedPreferences(
         R.string.sharedPreferences.toString(),
@@ -17,4 +19,5 @@ class BadgesPageViewModel(application: Application) : AndroidViewModel(applicati
     ).getInt(R.string.userID.toString(), -1)
 
     val badges = badgeRepository.getAcceptedBadgesByUserID(userID).asLiveData()
+    val user = userRepository.getUserByID(userID).asLiveData()
 }

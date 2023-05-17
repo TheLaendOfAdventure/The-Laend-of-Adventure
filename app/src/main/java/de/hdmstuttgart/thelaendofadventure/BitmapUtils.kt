b1b7886e9d646fd0d0c672b81ad9d816559497b2
SebastianMaier03 <Seb-Maier@outlook.de>
2023-05-18 01:50:53 +0200

    LOA-183 Add BitmapUtils to convert webp to Bitmap

---
+++ app/src/main/java/de/hdmstuttgart/thelaendofadventure/BitmapUtils.kt
@@ -0,0 +1,44 @@
+package de.hdmstuttgart.thelaendofadventure
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import androidx.annotation.DrawableRes
+import androidx.appcompat.content.res.AppCompatResources
+
+/**
+ * Utility class to work with bitmaps and drawables.
+ */
+object BitmapUtils {
+
+    /**
+     * Convert given drawable id to bitmap.
+     */
+    fun bitmapFromDrawableRes(context: Context, @DrawableRes resourceId: Int) =
+        convertDrawableToBitmap(AppCompatResources.getDrawable(context, resourceId))
+
+    private fun convertDrawableToBitmap(sourceDrawable: Drawable?): Bitmap? {
+        if (sourceDrawable == null) {
+            return null
+        }
+
+        return if (sourceDrawable is BitmapDrawable) {
+            sourceDrawable.bitmap
+        } else {
+            val drawableCopy = sourceDrawable.constantState?.newDrawable()?.mutate()
+            drawableCopy?.let { drawable ->
+                val bitmap = Bitmap.createBitmap(
+                    drawable.intrinsicWidth,
+                    drawable.intrinsicHeight,
+                    Bitmap.Config.ARGB_8888
+                )
+                val canvas = Canvas(bitmap)
+                drawable.setBounds(0, 0, canvas.width, canvas.height)
+                drawable.draw(canvas)
+                bitmap
+            }
+        }
+    }
+}

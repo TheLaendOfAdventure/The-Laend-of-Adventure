620bce3f04ae81be9addb887ac70241dd2d66663
ds217 <ds217@hdm-stuttgart.de>
2023-05-18 22:42:14 +0200

    LOA-261 implement logic for finished quest goal

---
+++ app/src/main/java/de/hdmstuttgart/thelaendofadventure/logic/QuestLogic.kt
@@ -0,0 +1,41 @@
+package de.hdmstuttgart.thelaendofadventure.logic
+
+import android.content.Context
+import android.util.Log
+import de.hdmstuttgart.thelaendofadventure.data.AppDataContainer
+import de.hdmstuttgart.thelaendofadventure.data.repository.QuestRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+class QuestLogic(context: Context) {
+
+    companion object {
+        private const val TAG = "QuestLogic"
+        private const val EXPERIENCE_PER_QUEST = 50
+    }
+    val context = context
+
+    private val questRepository: QuestRepository = AppDataContainer(context).questRepository
+
+    fun finishedQuestGoal(questID: Int, goalNumber: Int, userID: Int) {
+        CoroutineScope(Dispatchers.IO).launch {
+            questRepository.updateQuestProgressByUserID(
+                userID,
+                questID,
+                goalNumber + 1
+            )
+            Log.d(
+                TAG,
+                "User userID: $userID completed QuestGoal questID: $questID, questGoal: $goalNumber"
+            )
+
+            val progress = questRepository.getProgressForQuestByUserID(userID, questID).first()
+            if (progress.currentGoalNumber == progress.targetGoalNumber) {
+                Log.d(TAG, "User userID: $userID completed Quest questID: $questID")
+                UserLogic(context).addExperience(userID, EXPERIENCE_PER_QUEST)
+            }
+        }
+    }
+}

b25678eb7020a4135547951fc6bf758cae4f9dff
Aylin Alagöz <aa104@hdm-stuttgart.de>
2023-05-18 13:28:56 +0200

    LOA-252 rename updateQuestProgressByUserID method, add isQuestCompleted

--- app/src/main/java/de/hdmstuttgart/thelaendofadventure/data/offlinerepository/OfflineQuestRepository.kt
+++ app/src/main/java/de/hdmstuttgart/thelaendofadventure/data/offlinerepository/OfflineQuestRepository.kt
@@ -7,7 +7,9 @@ import de.hdmstuttgart.thelaendofadventure.data.dao.datahelper.QuestDetails
 import de.hdmstuttgart.thelaendofadventure.data.entity.ActionEntity
 import de.hdmstuttgart.thelaendofadventure.data.entity.QuestEntity
 import de.hdmstuttgart.thelaendofadventure.data.repository.QuestRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
 
 class OfflineQuestRepository(private val questDao: QuestDao) : QuestRepository {
 
@@ -33,11 +35,33 @@ class OfflineQuestRepository(private val questDao: QuestDao) : QuestRepository {
         Flow<List<ActionEntity>> =
         questDao.getUncompletedGoalsForQuestByUserID(userID, questID)
 
+    override suspend fun updateAndCheckQuestProgressByUserID(
+        userID: Int,
+        questID: Int,
+        goalNumber: Int
+    ): Boolean {
     override suspend fun getQuestForBadgeByUserID(userID: Int, badgeID: Int): Flow<List<Int>> =
         questDao.getQuestForBadgeByUserID(userID, badgeID)
 
     override suspend fun updateQuestProgressByUserID(userID: Int, questID: Int, goalNumber: Int) {
         questDao.updateQuestProgressByUserID(userID, questID, goalNumber)
+        return isQuestCompleted(userID, questID)
+    }
+
+    private suspend fun isQuestCompleted(userID: Int, questID: Int): Boolean {
+        val questProgress = questDao.getProgressForQuestByUserID(userID, questID)
+
+        var isCompleted = false
+
+        withContext(Dispatchers.IO) {
+            questProgress.collect { progress ->
+                val currentGoalNumber = progress.currentGoalNumber
+                val targetGoalNumber = progress.targetGoalNumber
+                isCompleted = currentGoalNumber == targetGoalNumber
+            }
+        }
+
+        return isCompleted
     }
 
     override suspend fun assignQuestToUser(userID: Int, questID: Int) {

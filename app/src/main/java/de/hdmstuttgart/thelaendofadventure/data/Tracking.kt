f6c197d544eeb5ebb15a8ef1bef84a04c13b3088
ds217 <ds217@hdm-stuttgart.de>
2023-05-18 22:40:25 +0200

    LOA-261 call follow up function

--- app/src/main/java/de/hdmstuttgart/thelaendofadventure/data/Tracking.kt
+++ app/src/main/java/de/hdmstuttgart/thelaendofadventure/data/Tracking.kt
@@ -5,10 +5,10 @@ import android.content.Context
 import android.os.Looper
 import android.util.Log
 import com.google.android.gms.location.*
-import com.google.android.gms.location.FusedLocationProviderClient
 import de.hdmstuttgart.the_laend_of_adventure.R
 import de.hdmstuttgart.thelaendofadventure.data.dao.datahelper.LocationGoal
 import de.hdmstuttgart.thelaendofadventure.data.repository.QuestRepository
+import de.hdmstuttgart.thelaendofadventure.logic.QuestLogic
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
 
@@ -18,16 +18,17 @@ class Tracking(context: Context) {
     companion object {
         private const val INTERVAL = 5000L
         private const val ALLOWED_DEVIATION = 0.0015
+        private const val TAG = "Tracking"
     }
 
-    private val tag = "Tracking"
     private var latitude: Double = 0.0
     private var longitude: Double = 0.0
+    private var context: Context = context
 
     private val questRepository: QuestRepository = AppDataContainer(context).questRepository
     val userID = context.getSharedPreferences(
         R.string.sharedPreferences.toString(),
-        Context.MODE_PRIVATE,
+        Context.MODE_PRIVATE
     ).getInt(R.string.userID.toString(), -1)
 
     private var fusedLocationClient: FusedLocationProviderClient =
@@ -52,32 +53,39 @@ class Tracking(context: Context) {
         fusedLocationClient.requestLocationUpdates(
             locationRequest,
             locationCallback,
-            Looper.getMainLooper(),
+            Looper.getMainLooper()
         )
     }
 
     suspend fun start() = CoroutineScope(Dispatchers.IO).launch {
-        val trackableQuest = questRepository.getLocationForAcceptedQuestsByUserID(userID)
-        trackableQuest.collect { list ->
-            CoroutineScope(Dispatchers.Default).launch {
-                while (isActive) {
-                    Log.d(tag, "Current userID: $userID")
-                    Log.d(tag, "Current list: $list")
-                    compareLocationGoal(list)
-                    delay(INTERVAL)
-                }
+        var trackableQuest = questRepository.getLocationForAcceptedQuestsByUserID(userID)
+        trackableQuest.collectLatest { list ->
+            while (isActive) {
+                Log.d(TAG, "Current userID: $userID")
+                Log.d(TAG, "Current list: $list")
+                compareLocationGoal(list)
+                delay(INTERVAL)
             }
         }
     }
 
     private fun compareLocationGoal(locationGoals: List<LocationGoal>) {
-        Log.d(tag, "Current longitude: $longitude Current latitude: $latitude")
+        Log.d(TAG, "Current longitude: $longitude Current latitude: $latitude")
         locationGoals.forEach { locationGoal ->
             if (locationGoal.latitude in latitude - ALLOWED_DEVIATION..latitude + ALLOWED_DEVIATION && // ktlint-disable max-line-length
                 locationGoal.longitude in longitude - ALLOWED_DEVIATION..longitude + ALLOWED_DEVIATION
             ) {
-                Log.d(tag, "finished for ${locationGoal.questID}")
-                // @todo call followup function for locationGoal.questID locationGoal.questGoalID
+                Log.d(
+                    TAG,
+                    "finished Goal ${locationGoal.currentGoalNumber} for questID: ${locationGoal.questID}"
+                )
+                var questLogic = QuestLogic(context)
+                questLogic.finishedQuestGoal(
+                    locationGoal.questID,
+                    locationGoal.currentGoalNumber,
+                    userID
+                )
+                return
             }
         }
     }

bc804db76269d6f8c07ad45998b44bf8344000c9
Konstantinos Gimoussiakakis <st172538@stud.uni-stuttgart.de>
2023-05-18 02:36:26 +0200

    LOA-202 add aditional list

--- app/src/main/java/de/hdmstuttgart/thelaendofadventure/ui/adapters/QuestAdapter.kt
+++ app/src/main/java/de/hdmstuttgart/thelaendofadventure/ui/adapters/QuestAdapter.kt
@@ -8,8 +8,11 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import de.hdmstuttgart.the_laend_of_adventure.R
 import de.hdmstuttgart.thelaendofadventure.data.entity.QuestEntity
+import de.hdmstuttgart.thelaendofadventure.data.entity.UserQuestEntity
 
-class QuestAdapter(private val mList: List<QuestEntity>) : RecyclerView.Adapter<QuestAdapter.ViewHolder>() { // ktlint-disable max-line-length
+class QuestAdapter(
+    private val questList: MutableList<Any>
+) : RecyclerView.Adapter<QuestAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         // inflates the card_view_design view
@@ -22,17 +25,19 @@ class QuestAdapter(private val mList: List<QuestEntity>) : RecyclerView.Adapter<
 
     // binds the list items to a view
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val quests = mList[position]
+        val quests = questList[position] as QuestEntity
+        val userQuest = questList[position] as UserQuestEntity
 
         // sets the text to the textview from our itemHolder class
         holder.textView.text = quests.name
 
         holder.progressBar.max = quests.targetGoalNumber
+        holder.progressBar.setProgress(userQuest.currentGoalNumber, true)
     }
 
     // return the number of the items in the list
     override fun getItemCount(): Int {
-        return mList.size
+        return questList.size
     }
 
     // Holds the views for adding it to image and text

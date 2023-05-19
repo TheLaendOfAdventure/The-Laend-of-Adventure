fadac67873461c642c790dd684cd7ba0c9462970
SebastianMaier03 <Seb-Maier@outlook.de>
2023-05-19 05:10:45 +0200

    LOA-183 Add quest Repository

--- app/src/main/java/de/hdmstuttgart/thelaendofadventure/ui/viewmodels/MainPageViewModel.kt
+++ app/src/main/java/de/hdmstuttgart/thelaendofadventure/ui/viewmodels/MainPageViewModel.kt
@@ -6,15 +6,21 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.asLiveData
 import de.hdmstuttgart.the_laend_of_adventure.R
 import de.hdmstuttgart.thelaendofadventure.data.AppDataContainer
+import de.hdmstuttgart.thelaendofadventure.data.repository.QuestRepository
 import de.hdmstuttgart.thelaendofadventure.data.repository.UserRepository
 
 class MainPageViewModel(application: Application) : AndroidViewModel(application) {
     private val userRepository: UserRepository = AppDataContainer(application).userRepository
-
+    private val questRepository: QuestRepository = AppDataContainer(application).questRepository
     val userID = application.getSharedPreferences(
         R.string.sharedPreferences.toString(),
         Context.MODE_PRIVATE
     ).getInt(R.string.userID.toString(), -1)
 
     val user = userRepository.getUserByID(userID).asLiveData()
+    val quests = questRepository.getUnacceptedQuestsByUserID(userID).asLiveData()
+
+    companion object {
+        private const val TAG = "MainPageViewModel"
+    }
 }

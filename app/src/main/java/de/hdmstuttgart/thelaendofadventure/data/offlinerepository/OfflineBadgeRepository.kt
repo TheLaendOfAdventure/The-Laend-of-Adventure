6e648f8fa7057304744ccef7f83e39cdd7c77531
Aylin Alagöz <aa104@hdm-stuttgart.de>
2023-05-19 02:34:36 +0200

    LOA-252 update updateBadgeProgressByUserID function and delete checkForBadgeProgress

--- app/src/main/java/de/hdmstuttgart/thelaendofadventure/data/offlinerepository/OfflineBadgeRepository.kt
+++ app/src/main/java/de/hdmstuttgart/thelaendofadventure/data/offlinerepository/OfflineBadgeRepository.kt
@@ -1,11 +1,13 @@
 package de.hdmstuttgart.thelaendofadventure.data.offlinerepository
 
 import de.hdmstuttgart.thelaendofadventure.data.dao.BadgeDao
+import de.hdmstuttgart.thelaendofadventure.data.dao.datahelper.BadgeDetails
 import de.hdmstuttgart.thelaendofadventure.data.dao.datahelper.Progress
 import de.hdmstuttgart.thelaendofadventure.data.entity.ActionEntity
 import de.hdmstuttgart.thelaendofadventure.data.entity.BadgeEntity
 import de.hdmstuttgart.thelaendofadventure.data.repository.BadgeRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.toList
 
 class OfflineBadgeRepository(private val badgeDao: BadgeDao) : BadgeRepository {
@@ -25,25 +27,17 @@ class OfflineBadgeRepository(private val badgeDao: BadgeDao) : BadgeRepository {
     override fun getUncompletedGoalsForBadgeByUserID(userID: Int, badgeID: Int):
         Flow<List<ActionEntity>> = badgeDao.getUncompletedGoalsForBadgeByUserID(userID, badgeID)
 
+    override fun getBadgesByUserIDAndQuestID(userID: Int, questID: Int):
+        Flow<List<BadgeDetails>> = badgeDao.getBadgesByUserIDAndQuestID(userID, questID)
+
     override suspend fun updateBadgeProgressByUserID(userID: Int, badgeID: Int, goalNumber: Int) {
-        val badgeProgress = badgeDao.getProgressForBadgeByUserID(userID, badgeID)
+        val badgeProgress = badgeDao.getProgressForBadgeByUserID(userID, badgeID).first()
 
-        badgeProgress.collect {
-            val currentGoalNumber = it.currentGoalNumber
-            val targetGoalNumber = it.targetGoalNumber
+        val currentGoalNumber = badgeProgress.currentGoalNumber
+        val targetGoalNumber = badgeProgress.targetGoalNumber
 
-            if (currentGoalNumber != targetGoalNumber) {
-                badgeDao.updateBadgeProgressByUserID(userID, badgeID, goalNumber)
-            }
-        }
-    }
-
-    private suspend fun checkForBadgeProgress(userID: Int, questID: Int) {
-        val allBadges = getAcceptedBadgesByUserID(userID)
-        allBadges.collect { badgesList ->
-            badgesList.forEach { badge ->
-
-            }
+        if (currentGoalNumber != targetGoalNumber) {
+            badgeDao.updateBadgeProgressByUserID(userID, badgeID, goalNumber)
         }
     }
 

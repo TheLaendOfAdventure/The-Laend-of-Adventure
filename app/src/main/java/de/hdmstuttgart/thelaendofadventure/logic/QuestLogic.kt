98979b917eaed8c63e5dcfa2586bd4528f3351ec
Hausch Johannes <jh285@hdm-stuttgart.de>
2023-05-26 11:06:49 +0200

    LOA-317 Update get userID yourself

--- app/src/main/java/de/hdmstuttgart/thelaendofadventure/logic/QuestLogic.kt
+++ app/src/main/java/de/hdmstuttgart/thelaendofadventure/logic/QuestLogic.kt
@@ -1,12 +1,14 @@
 package de.hdmstuttgart.thelaendofadventure.logic
 
-import ConversationPopupDialog
 import android.content.Context
 import android.util.Log
+import de.hdmstuttgart.the_laend_of_adventure.R
 import de.hdmstuttgart.thelaendofadventure.data.AppDataContainer
 import de.hdmstuttgart.thelaendofadventure.data.repository.ActionRepository
 import de.hdmstuttgart.thelaendofadventure.data.repository.BadgeRepository
 import de.hdmstuttgart.thelaendofadventure.data.repository.QuestRepository
+import de.hdmstuttgart.thelaendofadventure.ui.dialogpopup.ConversationPopupDialog
+import de.hdmstuttgart.thelaendofadventure.ui.dialogpopup.RiddlePopupDialog
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
@@ -24,18 +26,21 @@ class QuestLogic(private val context: Context) {
     private val badgeRepository: BadgeRepository = AppDataContainer(context).badgeRepository
     private val actionRepository: ActionRepository = AppDataContainer(context).actionRepository
 
+    val userID = context.getSharedPreferences(
+        R.string.sharedPreferences.toString(),
+        Context.MODE_PRIVATE
+    ).getInt(R.string.userID.toString(), -1)
+
     /**
      * Updates the quest goal progress for a specific user and checks if the quest is completed.
      * If the quest goal is completed, it also updates the corresponding badge progress for the user.
      *
      * @param questID The ID of the quest.
      * @param goalNumber The new goal number to set.
-     * @param userID The ID of the user.
      */
     fun finishedQuestGoal(
         questID: Int,
-        goalNumber: Int,
-        userID: Int
+        goalNumber: Int
     ) {
         CoroutineScope(Dispatchers.IO).launch {
             Log.d(
@@ -53,13 +58,47 @@ class QuestLogic(private val context: Context) {
             ) {
                 Log.d(TAG, "User userID: $userID completed Quest questID: $questID")
                 UserLogic(context).addExperience(userID, EXPERIENCE_PER_QUEST)
-                updateBadgeProgress(userID, questID)
+                updateBadgeProgress(userID)
             }
-            showConversation(userID, questID, goalNumber)
+            showConversation(questID, updatedGoalNumber)
         }
     }
 
-    private suspend fun updateBadgeProgress(userID: Int, questID: Int) {
+    private suspend fun showConversation(questID: Int, goalNumber: Int) {
+        val dialogPath: String? = actionRepository.getDialogPath(userID, goalNumber, questID)
+        Log.d(TAG, "dialogPath: $dialogPath")
+        if (dialogPath != null) {
+            withContext(Dispatchers.Main) {
+                val conversationPopupDialog = ConversationPopupDialog(context, dialogPath)
+                conversationPopupDialog.show()
+                conversationPopupDialog.setOnDismissListener {
+                    CoroutineScope(Dispatchers.IO).launch {
+                        checkRiddle()
+                        Log.d(TAG, "Launch RiddlePopup!")
+                    }
+                }
+            }
+        } else {
+            checkRiddle()
+        }
+    }
+
+    suspend fun checkRiddle() {
+        val riddleList = questRepository.getRiddleForAcceptedQuestsByUserID(userID).first()
+        Log.d(TAG, "RiddleList: $riddleList")
+        if (riddleList.isNotEmpty()) {
+            withContext(Dispatchers.Main) {
+                val riddlePopupDialog = RiddlePopupDialog(context, riddleList, userID)
+                riddlePopupDialog.show()
+                Log.d(TAG, "Riddle should be shown!")
+                riddlePopupDialog.setOnDismissListener {
+                    Log.d(TAG, "Riddle has been closed!")
+                }
+            }
+        }
+    }
+
+    private suspend fun updateBadgeProgress(questID: Int) {
         val badgeList = badgeRepository.getBadgesByUserIDAndQuestID(userID, questID).first()
 
         for (badge in badgeList) {
@@ -71,17 +110,6 @@ class QuestLogic(private val context: Context) {
             badgeRepository.updateBadgeProgressByUserID(userID, badgeID, currentGoalNumber + 1)
 
             Log.d(TAG, "Badge progress updated for User userID: $userID, badgeID: $badgeID")
-        }
-    }
-
-    private suspend fun showConversation(userID: Int, questID: Int, goalNumber: Int) {
-        val dialogPath: String? = actionRepository.getDialogPath(userID, goalNumber, questID)
-        Log.d(TAG, "dialogPath: $dialogPath")
-        if (dialogPath != null) {
-            withContext(Dispatchers.Main) {
-                val conversationPopupDialog = ConversationPopupDialog(context, dialogPath)
-                conversationPopupDialog.show()
-            }
         }
     }
 }

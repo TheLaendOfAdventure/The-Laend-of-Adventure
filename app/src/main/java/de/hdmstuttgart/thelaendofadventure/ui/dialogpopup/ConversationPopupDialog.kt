8f73692a02d4266f5007be9cf850b1af9344544d
Hausch Johannes <jh285@hdm-stuttgart.de>
2023-05-26 10:53:23 +0200

    LOA-317 Update conversation popup

--- app/src/main/java/de/hdmstuttgart/thelaendofadventure/ui/dialogpopup/ConversationPopupDialog.kt
+++ app/src/main/java/de/hdmstuttgart/thelaendofadventure/ui/dialogpopup/ConversationPopupDialog.kt
@@ -1,3 +1,4 @@
+package de.hdmstuttgart.thelaendofadventure.ui.dialogpopup
 import android.app.Dialog
 import android.content.Context
 import android.view.LayoutInflater
@@ -5,14 +6,30 @@ import de.hdmstuttgart.the_laend_of_adventure.databinding.ConversationPopupBindi
 
 class ConversationPopupDialog(private val context: Context, private val dialogPath: String) {
 
+    private lateinit var dialog: Dialog
+
     fun show() {
-        val dialog = Dialog(context)
+        dialog = Dialog(context)
         val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val binding = ConversationPopupBinding.inflate(inflater)
         dialog.setContentView(binding.root)
-
+        dialog.setCanceledOnTouchOutside(false)
+        dialog.setCancelable(false)
         binding.partnerTextView.text = "Dennis"
-
+        binding.questCardView.setOnClickListener {
+            dismissDialog()
+        }
         dialog.show()
+    }
+
+    private var dismissListener: (() -> Unit)? = null
+
+    fun setOnDismissListener(listener: () -> Unit) {
+        dismissListener = listener
+    }
+
+    private fun dismissDialog() {
+        dialog.dismiss()
+        dismissListener?.invoke()
     }
 }

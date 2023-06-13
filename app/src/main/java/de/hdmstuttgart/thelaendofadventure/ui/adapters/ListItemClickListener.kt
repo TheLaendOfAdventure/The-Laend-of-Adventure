be65b5c8eeb6c5a145d2b09bac2e161fd24c8f83
Konstantinos Gimoussiakakis <st172538@stud.uni-stuttgart.de>
2023-06-13 15:55:16 +0200

    LOA-338 add arrow logic

--- app/src/main/java/de/hdmstuttgart/thelaendofadventure/ui/adapters/ListItemClickListener.kt
+++ app/src/main/java/de/hdmstuttgart/thelaendofadventure/ui/adapters/ListItemClickListener.kt
@@ -1,14 +1,18 @@
 package de.hdmstuttgart.thelaendofadventure.ui.adapters
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.LinearLayout
+import de.hdmstuttgart.the_laend_of_adventure.R
 
-class ListItemClickListener(val innerInfo: LinearLayout) : View.OnClickListener {
+class ListItemClickListener(private val innerInfo: LinearLayout, private val arrow: ImageView) : View.OnClickListener { // ktlint-disable max-line-length
     override fun onClick(view: View) {
         if (innerInfo.visibility == View.VISIBLE) {
             innerInfo.visibility = View.GONE
+            arrow.setImageResource(R.drawable.down_arrow)
         } else {
             innerInfo.visibility = View.VISIBLE
+            arrow.setImageResource(R.drawable.up_arrow)
         }
     }
 }

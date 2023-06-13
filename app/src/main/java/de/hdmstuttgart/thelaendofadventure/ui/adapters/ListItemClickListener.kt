fac744c54d1a42315667badf6c1232def272061a
Konstantinos Gimoussiakakis <st172538@stud.uni-stuttgart.de>
2023-06-14 01:06:17 +0200

    LOA-338 add animation

--- app/src/main/java/de/hdmstuttgart/thelaendofadventure/ui/adapters/ListItemClickListener.kt
+++ app/src/main/java/de/hdmstuttgart/thelaendofadventure/ui/adapters/ListItemClickListener.kt
@@ -1,27 +1,40 @@
 package de.hdmstuttgart.thelaendofadventure.ui.adapters
 
-import android.transition.AutoTransition
+import android.transition.ChangeBounds
+import android.transition.Fade
 import android.transition.TransitionManager
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.cardview.widget.CardView
-import de.hdmstuttgart.the_laend_of_adventure.R
 
 class ListItemClickListener(
     private val cardView: CardView,
     private val innerInfo: LinearLayout,
     private val arrow: ImageView
-) : View.OnClickListener { // ktlint-disable max-line-length
+) : View.OnClickListener {
     override fun onClick(view: View) {
         if (innerInfo.visibility == View.VISIBLE) {
-            TransitionManager.beginDelayedTransition(cardView, AutoTransition())
-            arrow.setImageResource(R.drawable.down_arrow)
+            TransitionManager.beginDelayedTransition(
+                cardView.parent.parent as ViewGroup?,
+                ChangeBounds()
+            )
+            TransitionManager.beginDelayedTransition(innerInfo, Fade())
+            arrow.animate().rotation(0f)
             innerInfo.visibility = View.GONE
         } else {
-            TransitionManager.beginDelayedTransition(cardView, AutoTransition())
+            TransitionManager.beginDelayedTransition(
+                cardView.parent.parent as ViewGroup?,
+                ChangeBounds()
+            )
+            TransitionManager.beginDelayedTransition(innerInfo, Fade())
             innerInfo.visibility = View.VISIBLE
-            arrow.setImageResource(R.drawable.up_arrow)
+            arrow.animate().rotation(rotation)
         }
+    }
+
+    companion object {
+        private const val rotation = -180f
     }
 }

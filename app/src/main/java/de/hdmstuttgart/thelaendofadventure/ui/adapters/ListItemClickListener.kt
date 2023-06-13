8272d2af075f4fc4eeba514587c160db5d2bebe1
Konstantinos Gimoussiakakis <st172538@stud.uni-stuttgart.de>
2023-06-13 16:49:56 +0200

    LOA-338 add transistion animation

--- app/src/main/java/de/hdmstuttgart/thelaendofadventure/ui/adapters/ListItemClickListener.kt
+++ app/src/main/java/de/hdmstuttgart/thelaendofadventure/ui/adapters/ListItemClickListener.kt
@@ -1,16 +1,25 @@
 package de.hdmstuttgart.thelaendofadventure.ui.adapters
 
+import android.transition.AutoTransition
+import android.transition.TransitionManager
 import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
+import androidx.cardview.widget.CardView
 import de.hdmstuttgart.the_laend_of_adventure.R
 
-class ListItemClickListener(private val innerInfo: LinearLayout, private val arrow: ImageView) : View.OnClickListener { // ktlint-disable max-line-length
+class ListItemClickListener(
+    private val cardView: CardView,
+    private val innerInfo: LinearLayout,
+    private val arrow: ImageView
+) : View.OnClickListener { // ktlint-disable max-line-length
     override fun onClick(view: View) {
         if (innerInfo.visibility == View.VISIBLE) {
-            innerInfo.visibility = View.GONE
+            TransitionManager.beginDelayedTransition(cardView, AutoTransition())
             arrow.setImageResource(R.drawable.down_arrow)
+            innerInfo.visibility = View.GONE
         } else {
+            TransitionManager.beginDelayedTransition(cardView, AutoTransition())
             innerInfo.visibility = View.VISIBLE
             arrow.setImageResource(R.drawable.up_arrow)
         }

29b07c7b1066a7d44c41ccf0a63cfe1d3837b74f
Aylin Alagöz <aa104@hdm-stuttgart.de>
2023-06-16 12:36:03 +0200

    LOA 324 remove unused import

--- app/src/main/java/de/hdmstuttgart/thelaendofadventure/logic/TrackingLogic.kt
+++ app/src/main/java/de/hdmstuttgart/thelaendofadventure/logic/TrackingLogic.kt
@@ -6,7 +6,6 @@ import android.location.Location
 import android.os.Looper
 import android.util.Log
 import com.google.android.gms.location.* // ktlint-disable no-wildcard-imports
-import de.hdmstuttgart.the_laend_of_adventure.R
 import de.hdmstuttgart.thelaendofadventure.data.AppDataContainer
 import de.hdmstuttgart.thelaendofadventure.data.dao.datahelper.LocationGoal
 import de.hdmstuttgart.thelaendofadventure.data.repository.QuestRepository

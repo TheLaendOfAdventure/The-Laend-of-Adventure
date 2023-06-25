c9769f8245c31932acb75647e8a867d82f765b42
SebastianMaier03 <Seb-Maier@outlook.de>
2023-06-25 18:33:36 +0200

    LOA-270 Fix map

--- app/src/main/java/de/hdmstuttgart/thelaendofadventure/ui/helper/MapHelper.kt
+++ app/src/main/java/de/hdmstuttgart/thelaendofadventure/ui/helper/MapHelper.kt
@@ -49,7 +49,7 @@ class MapHelper(
 
     fun setUpMap() {
         mapview.getMapboxMap().loadStyleUri(
-            "asset://map/style.json"
+            context.getString(R.string.mapbox_styleURL)
         ) {
             val pointAnnotationList = prepareAnnotationMarker(mapview)
             val viewList = prepareViewAnnotation(pointAnnotationList, filteredQuestList)
